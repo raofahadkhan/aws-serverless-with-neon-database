a700f88262dc21764e01a8ef517ce9c4867475f4
RAO FAHAD <raofahad046@gmail.com>
2023-11-15 15:47:39 +0500

    Initial commit

---
+++ test/aws-serverless-with-neon.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsServerlessWithNeon from '../lib/aws-serverless-with-neon-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-serverless-with-neon-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsServerlessWithNeon.AwsServerlessWithNeonStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
